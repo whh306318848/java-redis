7bf5bba91e3d0021172baf84ee4ec59dde46ca38
DESKTOP-DU9LSVV\wuhaohua <whh306318848@126.com>
2022-03-07 23:03:16 +0800

    添加一个网上找到的springboot集成redis集群读写分离的示例项目，但是其存在的缺点是不能自动判断哪个是主节点，那个是从节点，需要手动配置

---
+++ springboot-redis-using/src/main/java/com/xue/study/RedisUsingStart.java
@@ -0,0 +1,23 @@
+package com.xue.study;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *  读写分离:
+ *    -- func1: 切换redisTemplate
+ *
+ *
+ */
+
+@SpringBootApplication
+public class RedisUsingStart {
+    private static final Logger LOGGER = LoggerFactory.getLogger("RedisUsingStart");
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedisUsingStart.class, args);
+        LOGGER.info("### welcome to springboot-redis-using ###");
+    }
+}
